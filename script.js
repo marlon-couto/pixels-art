83e18d23eaae6c7b11055caaee3ddbf74775d5cd
Marlon Couto <marloncouto199@gmail.com>
2022-10-04 20:41:18 -0300

    Adiciona persistência às cores geradas após pressionar o botão
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const color = document.querySelectorAll('.color');
 const randomButton = document.querySelector('#button-random-color');
 
+window.onload = function () {
+  initialColors();
+};
+
 function changeBoxColors() {
   for (let index = 1; index < color.length; index += 1) {
     color[index].style.backgroundColor = generateRGB();
   }
 }
-changeBoxColors();
 
 function generateRGB() {
   colorRGB = 'rgb(';
@@ -15,8 +18,30 @@ function generateRGB() {
   colorRGB += Math.floor(Math.random() * 255).toString() + ')';
   return colorRGB;
 }
-// console.log(generateRGB());
 
 randomButton.addEventListener('click', function () {
   changeBoxColors();
+  localStorage.setItem('colorPalette', JSON.stringify(storeColors()));
 });
+
+function storeColors() {
+  let colorPalette = [];
+
+  for (let index = 1; index < color.length; index += 1) {
+    colorPalette[index - 1] = color[index].style.backgroundColor;
+  }
+  return colorPalette;
+}
+
+function initialColors() {
+  if (localStorage.getItem('colorPalette')) {
+    let colorPalette;
+    colorPalette = JSON.parse(localStorage.getItem('colorPalette'));
+
+    for (let index = 1; index < color.length; index += 1) {
+      color[index].style.backgroundColor = colorPalette[index - 1];
+    }
+  } else {
+    changeBoxColors();
+  }
+}
